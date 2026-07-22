99c57a3b8f55fc36f7b297619dac0cab3526eb4c
sooryavanshianand <singhanandraj123@gmail.com>
2026-07-22 20:30:46 +0530

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -3,10 +3,31 @@ const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
 
 const app = express()
-
+const express = require('express');
+const aiRoutes = require('./routes/ai.routes')
+const cors = require('cors')
 app.use(cors({
-  origin: "*"
+  origin: [
+    "http://localhost:5173",
+    "https://dear-code-ochre.vercel.app"
+  ],
+  methods: ["GET", "POST"],
+  credentials: true
 }));
+const app = express()
+
+ 
+
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.use('/ai', aiRoutes)
+
+module.exports = app
 
 
 app.use(express.json())
